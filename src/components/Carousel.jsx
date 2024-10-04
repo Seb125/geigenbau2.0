ceeaaf74cfbc4ab0b3538640d6c01046af84020f
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2024-10-04 11:16:04 +0200

    kk

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,16 +3,26 @@ import { Paper, Box } from "@mui/material";
 
 function ImageCarousel({ images }) {
   return (
-    <Box
-      sx={{
-        width: "100%"
-      }}
-    >
-      <Carousel sx={{ width: "100%",  }} >
+    <Box>
+      <Carousel
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+      >
         {images.map((image, i) => (
           <Box
             component="img"
-            className="carousel-image"
+            sx={{
+              width: "100%",
+              position: "relative",
+              marginBottom: "50px",
+              
+            }}
             src={image}
             alt={`Slide ${i}`}
             key={i}
